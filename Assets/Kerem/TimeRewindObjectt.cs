b183bb3fc54608b0d7e574d1014531e850fe9e1a
KeremTancan <tancankerem149@gmail.com>
2023-07-06 18:18:07 +0300

    some update

--- Assets/Kerem/TimeRewindObjectt.cs
+++ Assets/Kerem/TimeRewindObjectt.cs
@@ -14,6 +14,8 @@ public class TimeRewindObjectt : MonoBehaviour
     private bool isRewinding = false;
     public static bool GeriSar;
     private List<ObjectState> objectStates = new List<ObjectState>();
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
 
     public Rigidbody rb;
 
@@ -65,45 +67,56 @@ public class TimeRewindObjectt : MonoBehaviour
 
     private void RecordObjectState()
     {
-        ObjectState state = new ObjectState();
-        state.position = transform.position;
-        state.rotation = transform.rotation;
-        objectStates.Add(state);
-        
+        if (transform.position != lastPosition || transform.rotation != lastRotation)
+        {
+            ObjectState state = new ObjectState();
+            state.position = transform.position;
+            state.rotation = transform.rotation;
+            objectStates.Add(state);
+
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+
+            Debug.Log("Kaydediliyor");
+            
+        }
     }
 
     private void RewindObject()
     {
-        if (objectStates.Count > 0)
+        if (objectStates.Count > 0 )
         {
-            GeriSar = true;
-
             ObjectState state = objectStates[objectStates.Count - 1];
             transform.position = state.position;
             transform.rotation = state.rotation;
-            objectStates.RemoveAt(objectStates.Count - 1);        
+            objectStates.RemoveAt(objectStates.Count - 1);
+            Debug.Log("Gerisarýyor");
+            GeriSar = true;
+            CameraFollow.isRunning = true;
         }
         
         else
         {
-            StopRewind();
-            GeriSar = false;
+            CameraFollow.isRunning = false;
+            Debug.Log("durdu");
+            StopRewind();          
         }
     }
 
     private void StartRewind()
     {
+        Debug.Log("baþladý");
         isRewinding = true;
-        rb.isKinematic = true;
-        CameraFollow.isRunning = true;
+        rb.isKinematic = true;        
+        GeriSar = true;
     }
 
     private void StopRewind()
     {
         isRewinding = false;
-        rb.isKinematic = true;
-        CameraFollow.isRunning = false;
-        
+        rb.isKinematic = true;  
+        GeriSar = false;
+
     }
 }
 

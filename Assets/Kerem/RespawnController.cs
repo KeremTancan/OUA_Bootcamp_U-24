cabac5b69ae46903b065e0f74f17c461d808e2d4
KeremTancan <tancankerem149@gmail.com>
2023-07-13 17:58:21 +0300

    Added: Checkpoint system

--- Assets/Kerem/RespawnController.cs
+++ Assets/Kerem/RespawnController.cs
@@ -8,7 +8,9 @@ public class RespawnController : MonoBehaviour
     private Vector3 previousPosition;
     private List<Vector3> positionHistory;
 
-   
+    private float time = 0.0f;
+    public float interpolationPeriod = 3f;
+
     void Start()
     {
         positionHistory = new List<Vector3>();
@@ -17,22 +19,25 @@ public class RespawnController : MonoBehaviour
 
     void FixedUpdate()
     {
+        time += Time.deltaTime;
+
         if (transform.position.y <= 13)
         {
             MoveToPreviousPosition();
         }
-        else 
+        else if (time >= interpolationPeriod)
         {
             positionHistory.Add(transform.position);
-           
+            time = 0.0f;
+            Debug.Log(positionHistory.Count);
         }
     }
 
     void MoveToPreviousPosition()
     {
-        transform.position = positionHistory[positionHistory.Count - 100];
-
+        transform.position = positionHistory[positionHistory.Count - 5];
         positionHistory.Clear();
+
     }
 
     

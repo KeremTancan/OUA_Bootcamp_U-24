8e3e00f41122052cc5168600d18253134972aabd
KeremTancan <tancankerem149@gmail.com>
2023-07-15 17:39:38 +0300

    baked lights

--- Assets/Kerem/PopUpMessage.cs
+++ Assets/Kerem/PopUpMessage.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PopUpMessage : MonoBehaviour
 {
     public GameObject text;
+    public string LevelName;
     private void Start()
     {
         text.SetActive(false);
@@ -13,15 +15,22 @@ public class PopUpMessage : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            text.SetActive(true);
-           
+            text.SetActive(true);           
+        }
+
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {          
+        {
             text.SetActive(false);
         }
     }
